316f45d881f9c662479f965e11362399003bd332
Sejun Park <sejunparks@gmail.com>
2024-04-30 12:18:41 -0400

    ProgressBar and Popup

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProgressStep from './ProgressSteps'
+import PopupMessage from './PopupMessage'
+import SideBar from './SideBar'
+import ToggleBtn from './ToggleBtn'
+
+const App = () => {
+  return (
+    <div>
+      <ProgressStep/>
+      <SideBar/>
+   
+   
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/PopupMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopupMessage = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default PopupMessage
