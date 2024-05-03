d611336190cfe65a0607e7c901114f988a3548e9
GabrielChan1 <neoco@techie.com>
2024-05-03 18:07:40 -0400

    Added ButtonGroup and CheckIcon components

---
+++ src/components/ButtonGroup/HRMButtonGroup.jsx
@@ -0,0 +1,44 @@
+import {ButtonGroup, Button} from '@mui/material';
+import PropTypes from 'prop-types';
+
+/**
+ * ButtonGroup component for both HRM and Onboarding applications.
+ * 
+ * Props:
+ * - buttonLabels<Array<String>>: Labels for each button in the button group. For each label in
+ *      'buttonLabels', a new button with that label will be added to the button group.
+ */
+export default function HRMButtonGroup({buttonLabels}) {
+    return (
+        <ButtonGroup 
+            variant="outlined"
+        >
+            {buttonLabels.map(label => {
+                return <Button
+                    sx={{
+                        fontFamily: `"Inter", sans-serif`,
+                        borderColor: "grey.400",
+                        color: "grey.900",
+                        borderRadius: 2,
+                        "&:hover": {
+                            cursor: 'pointer',
+                            borderColor: "grey.400"
+                        }
+                    }}>
+                        {label}
+                    </Button>
+            })}
+        </ButtonGroup>
+    );
+};
+
+//Control panel settings for storybook 
+HRMButtonGroup.propTypes = {
+    //Button labels
+    buttonLabels: PropTypes.array
+};
+
+//Default values for this component in storybook
+HRMButtonGroup.defaultProps = {
+    buttonLabels: ['Text', 'Text', 'Text']
+};
