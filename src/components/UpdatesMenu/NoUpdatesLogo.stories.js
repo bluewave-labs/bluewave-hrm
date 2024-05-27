f32d3cd7d11390cd280d0652dd4da0e582a08ce1
GabrielChan1 <neoco@techie.com>
2024-05-26 20:54:59 -0400

    Added storybook files

---
+++ src/components/UpdatesMenu/NoUpdatesLogo.stories.js
@@ -0,0 +1,16 @@
+import NoUpdatesLogo from './NoUpdatesLogo';
+
+//Storybook display settings
+export default {
+    title: 'HomeMenu/NoUpdatesLogo',
+    component: NoUpdatesLogo,
+    parameters: {
+        layout: 'centered'
+    },
+    tags: ['autodocs']
+};
+
+//Stories for each NoUpdatesLogo type
+export const Primary = {
+    args: {}
+};
