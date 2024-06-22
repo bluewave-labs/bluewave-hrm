680e9e272edecff1ca5276932a5465af41878e4d
ergunesra <167226329+ergunesra@users.noreply.github.com>
2024-06-22 15:32:19 -0400

    resolve check

--- src/components/textfields/TextFields.js
+++ src/components/textfields/TextFields.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './TextFields.css';
 import Box from '@mui/material/Box';
 import Grid from '@mui/system/Unstable_Grid';
-import {  IconButton, SvgIcon } from '@mui/material';
+import {  IconButton } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { styled } from '@mui/material/styles';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 const StyledButton = styled (Button) ({ 
-  color: '#667085', 
+  color: '#344054', 
   height: '34px',
+  textTransform: "none",
+  fontSize:'13px',
+  fontFamily:'Inter',
+  fontWeight:'regular',
   
 });
 
@@ -21,6 +27,19 @@ const CssWebTextField = styled(TextField)({
 
     width: '320px',
     height: '34px',
+
+    '& .MuiInputAdornment-root': {
+      color: '#667085', 
+      fontSize:'13px',
+      fontFamily:'Inter',
+      fontWeight:'regular',
+      
+    },
+    '& .MuiInputBase-input': {
+      color: '#667085', 
+      fontSize: '13px',
+      
+    },
     
     '& .MuiInputBase-root': { 
         m: 1, height:'34px' 
@@ -51,7 +70,14 @@ const CssMailTextField = styled(TextField)({
 
     width: '320px',
     height: '34px',
-    
+   
+    '& .MuiInputBase-input': {
+      color: '#667085', 
+      fontSize:'13px',
+      fontFamily:'Inter',
+      fontWeight:'regular',
+    },
+
     '& .MuiInputBase-root': { 
         m: 1, height:'34px' 
     },
@@ -69,7 +95,7 @@ const CssMailTextField = styled(TextField)({
         borderColor: '#D6BBFB',
       },
       '&.Mui-focused fieldset': {
-        borderColor: '#AEBDBD',
+        borderColor: '#D6BBFB',
       },
       '&.Mui-error fieldset': {
         borderColor: '#F04438', // Red color for error state
@@ -98,25 +124,14 @@ export const TextFields = (hasError) => {
                     placeholder='www.untitledui.com'
                     InputProps={{ 
                         startAdornment:
-                        <InputAdornment disableTypography position="end"> 
+                        <InputAdornment disableTypography position="start"> 
                             http://
                         </InputAdornment>, 
 
                         endAdornment:  
                         <InputAdornment disableTypography position="end"> 
                             <IconButton size='small'>
-                            <SvgIcon>
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_46_129)">
-                                    <path d="M6.06004 6C6.21678 5.55444 6.52614 5.17873 6.93334 4.93942C7.34055 4.7001 7.8193 4.61262 8.28483 4.69247C8.75035 4.77232 9.17259 5.01434 9.47676 5.37568C9.78093 5.73702 9.94741 6.19434 9.94671 6.66666C9.94671 8 7.94671 8.66666 7.94671 8.66666M8.00004 11.3333H8.00671M14.6667 8C14.6667 11.6819 11.6819 14.6667 8.00004 14.6667C4.31814 14.6667 1.33337 11.6819 1.33337 8C1.33337 4.3181 4.31814 1.33333 8.00004 1.33333C11.6819 1.33333 14.6667 4.3181 14.6667 8Z" stroke="#98A2B3" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_46_129">
-                                        <rect width="16" height="16" fill="white"/>
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                            </SvgIcon>
+                              <HelpOutlineIcon sx={{color:'#98A2B3', height: '16px', width: '16px', strokeWidth: '1.33px'}} ></HelpOutlineIcon>
                             </IconButton>
                         </InputAdornment>, 
                     }} 
@@ -147,11 +162,8 @@ export const TextFields = (hasError) => {
                 endAdornment:  
                 <InputAdornment disableTypography position="end"> 
                   <IconButton size='small'>
-                    <SvgIcon>
-                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.00004 5.33334V8.00001M8.00004 10.6667H8.00671M14.6667 8.00001C14.6667 11.6819 11.6819 14.6667 8.00004 14.6667C4.31814 14.6667 1.33337 11.6819 1.33337 8.00001C1.33337 4.31811 4.31814 1.33334 8.00004 1.33334C11.6819 1.33334 14.6667 4.31811 14.6667 8.00001Z" stroke="#F04438" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                      </svg>
-                    </SvgIcon>
+                    <ErrorOutlineIcon sx={{color:'#F04438', height: '16px', width: '16px', strokeWidth: '1px'}}></ErrorOutlineIcon>
+                    
                   </IconButton>
                 </InputAdornment>, 
               }} 
@@ -170,18 +182,7 @@ export const TextFields = (hasError) => {
                 endAdornment:  
                 <InputAdornment disableTypography position="end"> 
                   <IconButton size='small'>
-                    <SvgIcon>
-                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_46_129)">
-                          <path d="M6.06004 6C6.21678 5.55444 6.52614 5.17873 6.93334 4.93942C7.34055 4.7001 7.8193 4.61262 8.28483 4.69247C8.75035 4.77232 9.17259 5.01434 9.47676 5.37568C9.78093 5.73702 9.94741 6.19434 9.94671 6.66666C9.94671 8 7.94671 8.66666 7.94671 8.66666M8.00004 11.3333H8.00671M14.6667 8C14.6667 11.6819 11.6819 14.6667 8.00004 14.6667C4.31814 14.6667 1.33337 11.6819 1.33337 8C1.33337 4.3181 4.31814 1.33333 8.00004 1.33333C11.6819 1.33333 14.6667 4.3181 14.6667 8Z" stroke="#98A2B3" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                        </g>
-                        <defs>
-                          <clipPath id="clip0_46_129">
-                            <rect width="16" height="16" fill="white"/>
-                          </clipPath>
-                        </defs>
-                      </svg>
-                    </SvgIcon>
+                  <HelpOutlineIcon sx={{color:'#98A2B3', height: '16px', width: '16px', strokeWidth: '1.33px'}} ></HelpOutlineIcon>
                   </IconButton>
                 </InputAdornment>, 
               }}                        
@@ -200,27 +201,14 @@ export const TextFields = (hasError) => {
                         endAdornment:  
                         <InputAdornment disableTypography position="end"> 
                             <IconButton size='small'>
-                            <SvgIcon>
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_46_129)">
-                                    <path d="M6.06004 6C6.21678 5.55444 6.52614 5.17873 6.93334 4.93942C7.34055 4.7001 7.8193 4.61262 8.28483 4.69247C8.75035 4.77232 9.17259 5.01434 9.47676 5.37568C9.78093 5.73702 9.94741 6.19434 9.94671 6.66666C9.94671 8 7.94671 8.66666 7.94671 8.66666M8.00004 11.3333H8.00671M14.6667 8C14.6667 11.6819 11.6819 14.6667 8.00004 14.6667C4.31814 14.6667 1.33337 11.6819 1.33337 8C1.33337 4.3181 4.31814 1.33333 8.00004 1.33333C11.6819 1.33333 14.6667 4.3181 14.6667 8Z" stroke="#98A2B3" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_46_129">
-                                        <rect width="16" height="16" fill="white"/>
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                            </SvgIcon>
+                            <HelpOutlineIcon sx={{color:'#98A2B3', height: '16px', width: '16px', strokeWidth: '1.33px'}} ></HelpOutlineIcon>
                             </IconButton>
                             <Divider orientation="vertical" flexItem />
-                            <StyledButton startIcon={<ContentCopyIcon />}>
-                                COPY
+                            <StyledButton startIcon={<ContentCopyIcon sx={{color:'#667085', height: '16px', width: '16px', strokeWidth: '1.5px'}} ></ContentCopyIcon>}>
+                                Copy
                             </StyledButton>
                         </InputAdornment>, 
                     }} 
-                    
-                
                 />  
                 <p className="hint">This is a hint text to help user.</p>       
           </div>
