3a4061aef68227a0524b418bd070267f970cddda
Olusegun Awowole <olusegun.sm@yahoo.com>
2024-07-03 21:41:22 -0400

    Updated database schema.

---
+++ models/appUser.js
@@ -0,0 +1,71 @@
+const bcrypt = require("bcrypt");
+
+module.exports = (sequelize, Sequelize) => {
+  return sequelize.define(
+    "hrmUser",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        set(value) {
+          const newValue = value ? value.toLowerCase() : value;
+          this.setDataValue("email", newValue);
+        },
+        get() {
+          const rawValue = this.getDataValue("email");
+          return rawValue ? rawValue.toLowerCase() : null;
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        set(value) {
+          const salt = bcrypt.genSaltSync();
+          const hashedPassword = bcrypt.hashSync(value, salt);
+          this.setDataValue("password", hashedPassword);
+        },
+      },
+      empId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        unique: true,
+      },
+      permissionId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      passwordChangedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      passwordResetToken: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      passwordResetTokenExpiresAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+};
