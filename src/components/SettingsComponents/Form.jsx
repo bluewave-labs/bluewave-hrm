dde0068ec3a308e5268d8905c5ef67602b095979
Malena Aguiar <malenaaguiar@Malenas-MacBook-Pro.local>
2024-07-08 10:19:07 -0600

    Added react hook form and started form component

--- src/components/SettingsComponents/Form.jsx
+++ src/components/SettingsComponents/Form.jsx
@@ -1,9 +1,48 @@
-import { FormControl } from '@mui/base/FormControl';
+import { useForm } from "react-hook-form";
+import Stack from "@mui/system/Stack";
+import { FormControl, TextField } from "@mui/material";
+import HRMButton from "../Button/HRMButton";
+import "./settings.css";
+
 export default function Form() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <FormControl defaultValue="" required>
-      <label>Name</label>
-      <input placeholder="Write your name here" />
-    </FormControl>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl>
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+          sx={{ marginBottom: "25px" }}
+        >
+          <label>Company name</label>
+          <TextField
+            name="companyName"
+            inputProps={register}
+            color="secondary"
+          />
+        </Stack>
+      </FormControl>
+      <FormControl>
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+          sx={{ marginBottom: "25px" }}
+        >
+          <label>Company website</label>
+          <TextField
+            name="companyName"
+            inputProps={register}
+            color="secondary"
+          />
+        </Stack>
+      </FormControl>
+      <Stack direction="row" alignItems="center">
+        <HRMButton mode="primary">Save changes</HRMButton>
+      </Stack>
+    </form>
   );
 }
