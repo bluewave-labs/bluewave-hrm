ee870aae43cf1f0360f3aef9442bef80e99b0cbf
Malena Aguiar <malenaaguiar@Malenas-MacBook-Pro.local>
2024-07-16 18:36:00 -0600

    ui table and integration data departments tab

--- src/components/SettingsPage/DepartmentsTabContent.jsx
+++ src/components/SettingsPage/DepartmentsTabContent.jsx
@@ -1,110 +1,75 @@
-import Box from '@mui/system/Box';
-import Stack from '@mui/system/Stack';
-import TuneIcon from '@mui/icons-material/Tune';
-import UpcomingTimeOffTable from '../TimeOffPage/UpcomingTimeOffTable';
-import PagesNavBar from '../UpdatesPage/PagesNavBar';
-import MenuToggleButton from '../BasicMenus/MenuToggleButton';
-import NoContentComponent from '../UpdatesPage/NoContentComponent';
-import Label from '../Label/Label';
-import { useState } from 'react';
-import { colors, fonts } from '../../Styles';
-import PropTypes from 'prop-types';
-import DepartmentsTable from './DepartmentsTable';
+import Box from "@mui/system/Box";
+import Stack from "@mui/system/Stack";
+import PagesNavBar from "../UpdatesPage/PagesNavBar";
+import Label from "../Label/Label";
+import { colors, fonts } from "../../Styles";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import DepartmentsTable from "./DepartmentsTable";
 
 /**
- * Displays the content for the History tab in the time off menu which includes the complete time
- * off history for the current user. Displays up to 10 periods of time off at a time along with 
- * a customization menu and controls for navigating between pages of components.
- * 
+ * Displays the content for the Board tab in the time off menu which includes the available
+ * time off per policy and any upcoming periods of time off scheduled.
+ *
  * Props:
- * - departments<Array<Object>>: List of objects containing information of upcoming periods
+ * - policies<Array<Object>>: List of objects containing policy information to be displayed.
+ *
+ * - timeOffPeriods<Array<Object>>: List of objects containing information of upcoming periods
  *      of time off.
- * 
+ *
  * - style<Object>: Optional prop for adding further inline styling.
  *      Default: {}
  */
-export default function DepartmentsTabContent({departments, style}) {
-    const [currentPage, setCurrentPage] = useState(1);  //The current page number
-    //Flags for determining which buttons in the "customize" dropdown are selected
-    const [nameSelected, setNameSelected] = useState(false);
-    const [peopleSelected, setPeopleSelected] = useState(false);
+export default function DepartmentsTabContent({ departments, style }) {
+  const [currentPage, setCurrentPage] = useState(1); //The current page number
 
+  //Function for changing the page number
+  function handlePage(n) {
+    if (n > 0 && n <= Math.ceil(departments.length / 10)) {
+      setCurrentPage(n);
+    }
+  }
 
-    console.log("departments");
-    console.log(departments);
-
-    //Function for changing the page number
-    function handlePage(n) {
-        if (n > 0 && n <= Math.ceil(departments.length / 10)) {
-            setCurrentPage(n);
-        }
-    };
-
-    return (
-        <Box sx={{...{
-            color: colors.darkGrey,
-            fontFamily: fonts.fontFamily
-        }, ...style}}>
-            {/*Time off header*/}
-            <Stack 
-                direction="row" 
-                alignItems="center" 
-                justifyContent="space-between"
-                sx={{
-                    marginY: "40px"
-                }}
-            >
-                <Stack direction="row" alignItems="center" spacing={3}>
-                    <h3>Departments</h3>
-                    <Label 
-                        mode="brand" 
-                        label={departments.length} 
-                        style={{borderRadius: "50%"}} 
-                    />
-                </Stack>
-                {/*Customize button*/}
-                {departments.length > 0 &&
-                    <DepartmentsTable departments={departments}/>
-                }
-            </Stack>
-            {/*If there are periods of time off, display the time off period list and navbar */}
-            {(departments.length > 0) ?
-                <>
-                    {/*Upcoming time off table*/}
-                    {/* <UpcomingTimeOffTable 
-                        departments={departments} 
-                        editFlag={true} 
-                        teamFlag={false} 
-                        style={{marginBottom: "30px"}}
-                    /> */}
-                    {/*Upcoming time off navbar*/}
-                    {departments.length > 10 &&
-                        <PagesNavBar 
-                            numOfEntries={departments.length} 
-                            currentPage={currentPage} 
-                            handlePage={handlePage}
-                        /> 
-                    }  
-                </> :
-                <>
-                    {/*Otherwise, display a message that there is no history*/}
-                    <NoContentComponent>
-                        <h3>There is no time off history</h3>
-                        <p>Any updates about your time off history will be shown here.</p>
-                    </NoContentComponent>
-                </>
-            }
-        </Box>
-    );
-};
+  return (
+    <Box
+      sx={{
+        ...{
+          marginTop: "40px",
+          color: colors.darkGrey,
+          fontFamily: fonts.fontFamily,
+        },
+        ...style,
+      }}
+    >
+      {/*header and table*/}
+      <h3 style={{ marginBottom: "40px" }}>Departments</h3>
+      <DepartmentsTable departments={departments} sx={{ marginBottom: "40px" }} />
+      {/*header*/}
+      {departments.length > 0 ? (
+        <>
+          {/*Upcoming time off navbar*/}
+          {departments.length > 10 && (
+            <PagesNavBar
+              numOfEntries={departments.length}
+              currentPage={currentPage}
+              handlePage={handlePage}
+            />
+          )}
+        </>
+      ) : (
+        <p>There is no departments right now.</p>
+      )}
+    </Box>
+  );
+}
 
 //Control panel settings for storybook
 DepartmentsTabContent.propTypes = {
-    //Periods of time off to be displayed
-    departments: PropTypes.arrayOf(PropTypes.object)
+  //Time off departments to be displayed
+  departments: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
 };
 
 //Default values for this component
 DepartmentsTabContent.defaultProps = {
-    style: {}
+  style: {},
 };
--- src/components/SettingsPage/DepartmentsTable.jsx
+++ src/components/SettingsPage/DepartmentsTable.jsx
@@ -1,133 +1,114 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/system/Stack';
-import Avatar from '@mui/material/Avatar';
-import { styled } from '@mui/system';
-import PropTypes from 'prop-types';
-import HRMButton from '../Button/HRMButton';
-import Label from '../Label/Label';
-import { colors, fonts } from '../../Styles';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import Typography from "@mui/material/Typography";
+import TableRow from "@mui/material/TableRow";
+import { styled } from "@mui/system";
+import { colors, fonts } from "../../Styles";
+import PropTypes from "prop-types";
+import Stack from "@mui/system/Stack";
+import HRMButton from "../Button/HRMButton";
 
-/**
- * Menu component for listing upcoming scheduled periods of time off
- * 
- * Props:
- * - timeOffPeriods<Array<Object>>: List of objects containing information of upcoming periods 
- *      of time off.
- * 
- * - editFlag<Boolean>: Boolean determining if time off periods can be edited or deleted.
- *      Default: false
- * 
- * - teamFlag<Boolean>: Boolean determining if the time off periods belong to other team members.
- *      Default: false
- * 
- * - style<Object>: Optional prop for adding further inline styling.
- *      Default: {}
- */
-export default function DepartmentsTable({departments, editFlag, teamFlag, style}) {
-    //Custom style elements
-    const TableHeaderCell = styled(TableCell)({
-        color: colors.darkGrey, 
-        paddingTop: "10px",
-        paddingBottom: "10px"
-    });
+const TextHeader = styled(Typography)({
+  fontSize: "14px",
+  fontWeight: "500",
+  lineHeight: "18px",
+  color: "#475467",
+});
 
-    const TableBodyCell = styled(TableCell)({
-        color: colors.darkGrey,
-        paddingTop: "25px",
-        paddingBottom: "25px"
-    });
+const Text = styled(Typography)({
+  fontSize: "16px",
+  fontWeight: "500",
+  lineHeight: "20px",
+  color: "#101828",
+});
+export default function DepartmentsTable({ departments, style }) {
+  //Custom style elements
+  const TableHeaderCell = styled(TableCell)({
+    color: colors.darkGrey,
+    paddingTop: "10px",
+    paddingBottom: "10px",
+  });
 
-    const StyledChip = styled(Chip)({
-        border: "1px solid #EAECF0",
-        backgroundColor: "#F9FAFB"
-    });
+  const TableBodyCell = styled(TableCell)({
+    color: colors.darkGrey,
+    paddingTop: "25px",
+    paddingBottom: "25px",
+  });
 
-    return (
-        <TableContainer sx={{...{
-            minWidth: "885px",
-            fontFamily: fonts.fontFamily
-        }, ...style}}>
-            <Table>
-                {/*Table header*/}
-                <TableHead>
-                    <TableRow sx={{backgroundColor: "#F9FAFB"}}>
-                        <TableHeaderCell><b>Name</b></TableHeaderCell>
-                        <TableHeaderCell><b>People</b></TableHeaderCell>
-                    </TableRow>
-                </TableHead>
-                {/*Time off information*/}
-                <TableBody>
-                    {departments.map((department) => (
-                        <TableRow>
-                            {/*Starting and ending dates of time off department*/}
-                            <TableBodyCell sx={{paddingLeft: "25px"}}>
-                                <StyledChip label={<b>{department.departmentName}</b>} />
-                            </TableBodyCell>
-                            <TableBodyCell>
-                                <StyledChip label={<b>{department.departmentManagerId}</b>} />
-                            </TableBodyCell>
-                            {editFlag && 
-                                <TableBodyCell>
-                                    <Stack direction="row" alignItems="center" justifyContent="flex-end">
-                                        <HRMButton mode="tertiary"><b>Delete</b></HRMButton>
-                                        <HRMButton mode="tertiary">
-                                            <a 
-                                                href="#" 
-                                                style={{
-                                                    color: "#7F56D9", 
-                                                    textDecoration: "none", 
-                                                    fontWeight: "bold"
-                                                }}
-                                            >
-                                                Edit
-                                            </a>
-                                        </HRMButton>
-                                    </Stack>
-                                </TableBodyCell>
-                            }
-                            {/*The status of the time off period*/}
-                            {/* {teamFlag && 
-                                <TableBodyCell>
-                                    <Label
-                                        mode="status" 
-                                        dot={
-                                            (period.status === "Approved") ? "green" :
-                                            (period.status === "Waiting") ? "orange" :
-                                            (period.status === "Rejected") ? "red" : null
-                                        }
-                                        label={period.status}
-                                    />
-                                </TableBodyCell>
-                            } */}
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-};
+  return (
+    <TableContainer
+      sx={{
+        ...{
+          minWidth: "885px",
+          fontFamily: fonts.fontFamily,
+        },
+        ...style,
+      }}
+    >
+      <Table>
+        {/*Table header*/}
+        <TableHead>
+          <TableRow sx={{ backgroundColor: "#F9FAFB" }}>
+            <TableHeaderCell sx={{ width: "50%", paddingLeft: "25px" }}>
+              <TextHeader>Name</TextHeader>
+            </TableHeaderCell>
+            <TableHeaderCell>
+              <TextHeader>People</TextHeader>
+            </TableHeaderCell>
+            <TableHeaderCell></TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        {/*Policy information*/}
+        <TableBody>
+          {departments.map((department) => (
+            <TableRow>
+              <TableBodyCell sx={{ width: "50%", paddingLeft: "25px" }}>
+                <Text>{department.departmentName}</Text>
+              </TableBodyCell>
+              <TableBodyCell>
+                <Text>{department.departmentManagerId}</Text>
+              </TableBodyCell>
+              <TableBodyCell>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="flex-start"
+                >
+                  <HRMButton mode="tertiary">
+                    <b>Delete</b>
+                  </HRMButton>
+                  <HRMButton mode="tertiary">
+                    <a
+                      href="#"
+                      style={{
+                        color: "#7F56D9",
+                        textDecoration: "none",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Edit
+                    </a>
+                  </HRMButton>
+                </Stack>
+              </TableBodyCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 //Control panel settings for storybook
 DepartmentsTable.propTypes = {
-    //Periods of time off to be displayed
-    departments: PropTypes.arrayOf(PropTypes.object),
-
-    //Boolean determining if time off periods can be edited or deleted in this menu
-    editFlag: PropTypes.bool,
-
-    //Boolean determining if the time off periods of other users are being displayed in this menu
-    teamFlag: PropTypes.bool
+  //Time off policies to be displayed
+  departments: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
 };
 
 //Default values for this component
 DepartmentsTable.defaultProps = {
-    editFlag: false,
-    teamFlag: false,
-    style: {}
+  style: {},
 };
