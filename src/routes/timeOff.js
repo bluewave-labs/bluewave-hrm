2d65fe83c36aeac51a362e74567e0590b1eaad9e
Shidrath Rahman <shidrathrahman365@gmail.com>
2024-09-23 11:51:07 -0400

    added business logic to creation and deletion of new/old time offs

--- src/routes/timeOff.js
+++ src/routes/timeOff.js
@@ -2,9 +2,15 @@ const controller = require("../controllers/timeOff");
 const { requireAuth } = require("../../config/authJwt");
 
 module.exports = (router) => {
-  router.route("/timeoffs").get(controller.showAll);
-  router.route("/timeoffs/:id").post(controller.showOne);
-  router.route("/timeoffs").post(controller.createRecord);
-  router.route("/timeoffs").put(controller.updateRecord);
-  router.route("/timeoffs/:id").delete(controller.deleteRecord);
+  router.route("/timeoffs").get(requireAuth, controller.showAll);
+  router.route("/timeoffs/:id").post(requireAuth, controller.showOne);
+  router.route("/timeoffs").post(requireAuth, controller.createRecord);
+  router.route("/timeoffs").put(requireAuth, controller.updateRecord);
+  router.route("/timeoffs/:id").delete(requireAuth, controller.deleteRecord);
+  router.route("/timeoffs/deletion/initiate").post(requireAuth, controller.initiateDeletion);
+  router.route("/timeoffs/deletion/confirm").delete(requireAuth, controller.confirmDeletion);
+  router.route("/timeoffs/renewaldate/set").post(requireAuth, controller.setRenewalDate);
+  router.route("/timeoffs/renewaldate/get").get(requireAuth, controller.getRenewalDate);
 };
+
+
